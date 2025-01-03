f6b77da43073e8f6bf399465b6dde63151ecb118
Ananth1082 <ananthshetty203@gmail.com>
2025-01-03 06:37:22 +0530

    [feat] added queueing system

--- backend/server.go
+++ backend/server.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"math/rand"
 	"net/http"
+	"time"
 
 	"github.com/Ananth1082/arenas/prisma/db"
 	"github.com/labstack/echo/v4"
@@ -11,22 +12,51 @@ import (
 	"golang.org/x/net/websocket"
 )
 
-func hello(c echo.Context) error {
+var MMQueue = make(chan string, 1)
+var isDone = make(chan *db.MatchesModel)
+
+func matchMaking(c echo.Context) error {
 	websocket.Handler(func(ws *websocket.Conn) {
 		defer ws.Close()
-		for {
-			err := websocket.Message.Send(ws, "Hello, Client!")
-			if err != nil {
-				c.Logger().Error(err)
-			}
 
-			msg := ""
-			err = websocket.Message.Receive(ws, &msg)
-			if err != nil {
-				c.Logger().Error(err)
-			}
-			fmt.Printf("%s\n", msg)
+		// Greet the client
+		if err := websocket.Message.Send(ws, "Hello, Client!"); err != nil {
+			c.Logger().Error("Error sending greeting:", err)
+			return
 		}
+
+		// Receive user info
+		player1ID := ""
+		err := websocket.Message.Receive(ws, &player1ID)
+		if err != nil {
+			c.Logger().Error(err)
+		}
+
+		select {
+		case player2ID := <-MMQueue:
+			games, err := client.Games.FindMany().Exec(context.Background())
+			if err != nil || len(games) == 0 {
+				websocket.Message.Send(ws, `{"error": "No games available"}`)
+				return
+			}
+			randGame := games[rand.Intn(len(games))]
+			match, err := client.Matches.CreateOne(
+				db.Matches.Player1.Link(db.User.ID.Equals(player1ID)),
+				db.Matches.Player2.Link(db.User.ID.Equals(player2ID)),
+				db.Matches.Game.Link(db.Games.ID.Equals(randGame.ID)),
+				db.Matches.Time.Set(db.DateTime(time.Now().Add(5*time.Minute))),
+			).Exec(context.Background())
+			if err != nil {
+				websocket.Message.Send(ws, `{"error": "Error creating match"}`)
+				return
+			}
+			isDone <- match
+			websocket.JSON.Send(ws, match)
+		case MMQueue <- player1ID:
+			match := <-isDone
+			websocket.JSON.Send(ws, match)
+		}
+
 	}).ServeHTTP(c.Response(), c.Request())
 	return nil
 }
@@ -40,8 +70,6 @@ func server() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	e.GET("/ws", hello)
-
 	e.POST("/user", func(c echo.Context) error {
 		req := new(User)
 		c.Bind(req)
@@ -53,5 +81,6 @@ func server() {
 		return c.JSON(http.StatusOK, echo.Map{"msg": "user created", "user": newUser})
 	})
 
+	e.GET("/ws/match-making", matchMaking)
 	e.Logger.Fatal(e.Start(":8080"))
 }
